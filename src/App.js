4b1ff8e452ad89d283c378c4a3608cc43975d28e
Rajat Lilani <68480562+rajatlilani@users.noreply.github.com>
2023-08-01 13:40:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,50 @@
+array of objects
+
+const data = [
+  { JobGroupName: "a", JobType: 1, JobTitle: 3 },
+  { JobGroupName: "a", JobType: 1, JobTitle: 4 },
+  { JobGroupName: "a", JobType: 2, JobTitle: 5 },
+  { JobGroupName: "b", JobType: 3, JobTitle: 6 },
+  { JobGroupName: "a", JobType: 4, JobTitle: 1 },
+  { JobGroupName: "b", JobType: 5, JobTitle: 5 },
+];
+
+const output = data.reduce((result, item, index) => {
+  const existingGroup = result.find((group) => group.JobGroupName === item.JobGroupName);
+
+  if (existingGroup) {
+    const existingType = existingGroup.__children.find((child) => child.JobType === item.JobType);
+    if (!existingType && item.JobType !== null && item.JobTitle !== null) {
+      existingGroup.__children.push({ JobType: item.JobType, JobTitle: item.JobTitle });
+    }
+  } else {
+    const groupItem = {
+      JobGroupName: item.JobGroupName,
+      __children: [],
+    };
+
+    if (index === 0) {
+      groupItem.JobType = null;
+      groupItem.JobTitle = null;
+    }
+
+    if (item.JobType !== null && item.JobTitle !== null) {
+      groupItem.__children.push({ JobType: item.JobType, JobTitle: item.JobTitle });
+    }
+
+    result.push(groupItem);
+  }
+
+  return result;
+}, []);
+
+const outputArray = Object.values(output);
+console.log(outputArray);
+
+
+
+
+
 const data = [
   { JobGroupName: "a", JobType: 1, JobTitle: 3 },
   { JobGroupName: "a", JobType: 1, JobTitle: 4 },
