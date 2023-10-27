edd4b3197af73e1c0630cacf61229dad1e741331
Rajat Lilani <68480562+rajatlilani@users.noreply.github.com>
2023-10-27 10:43:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
+export function requestNotificationPermission() {
+  if (Notification.permission !== "granted") {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        console.log("Permission granted for notifications");
+      }
+    });
+  }
+}
+
+export function sendNotification(title, options, timeout) {
+  if (Notification.permission === "granted") {
+    setTimeout(() => {
+      new Notification(title, options);
+    }, timeout);
+  }
+}
+
+
+
+
+
+const mockHub = {
+  on: (eventName, callback) => {
+    const events = {
+      ReceiveMessage: "This is a mock message",
+    };
+
+    if (events[eventName]) {
+      callback(events[eventName]);
+    }
+  },
+  start: () => Promise.resolve(),
+};
+
+export default mockHub;
+
+
+
+
+
+import React, { useEffect } from "react";
+import { requestNotificationPermission, sendNotification } from "./PushNotification";
+import mockHub from "./MockSignalRHub";
+
+const App = () => {
+  useEffect(() => {
+    requestNotificationPermission();
+    mockHub.start();
+
+    const notificationInterval = setInterval(() => {
+      mockHub.on("ReceiveMessage", (message) => {
+        sendNotification("New Message", { body: message }, 0);
+      });
+    }, 10000);
+
+    return () => clearInterval(notificationInterval);
+  }, []);
+
+  return <div>React App with Mock SignalR and Web Push Notifications</div>;
+};
+
+export default App;
+
+
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
